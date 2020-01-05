c7262b278e03d871bb643cc0062c841980e57898
wangooo <https://github.com/wangooo/album.git>
2020-01-05 19:04:27 +0800

    数据库操作

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,48 +5,48 @@ var Goods = require('../models/goods');
 var Auser = require('../models/user');
 
 //连接MongoDB数据库
-mongoose.connect('mongodb://127.0.0.1:27017/taobao',{useNewUrlParser:true});
+mongoose.connect('mongodb://127.0.0.1:27017/taobao', { useNewUrlParser: true });
 
-mongoose.connection.on("connected",function(){
+mongoose.connection.on("connected", function () {
   console.log("连接数据库成功");
 });
 
-mongoose.connection.on("error",function(){
+mongoose.connection.on("error", function () {
   console.log("连接数据库失败");
 });
 
-mongoose.connection.on("disconnected",function(){
+mongoose.connection.on("disconnected", function () {
   console.log("disconnected")
 });
 
 console.log("-1");
 
-router.post("/login",function(req,res,next){
+router.post("/login", function (req, res, next) {
   console.log("000");
-  var param={
-    userName:req.body.tuserName,
-    userPwd:req.body.tuserPwd
+  var param = {
+    userName: req.body.tuserName,
+    userPwd: req.body.tuserPwd
   };
-  Auser.findOne(param,function(err,doc){
+  Auser.findOne(param, function (err, doc) {
     console.log("111");
-    if(err){
+    if (err) {
       res.json({
-        status:'1',
-        msg:"错啦"
+        status: '1',
+        msg: "错啦"
       })
     }
-    else{
-      if(doc){
-        res.cookie("userId",doc.userId,{
-          path:'/',
-          maxAge:1000*60*60
+    else {
+      if (doc) {
+        res.cookie("userId", doc.userId, {
+          path: '/',
+          maxAge: 1000 * 60 * 60
         });
         // req.session.user=doc;
         res.json({
-          status:'0',
-          msg:'',
-          result:{
-            userName:doc.userName
+          status: '0',
+          msg: '',
+          result: {
+            userName: doc.userName
           }
         })
       }
@@ -54,38 +54,28 @@ router.post("/login",function(req,res,next){
   })
 });
 
-router.get("/getUsers",function(req,res,next){
-  res.json({
-    status:'0',
-    msg:'',
-    result:{
-      list:[1,2,3]
-    }
-  })
-});
-
-router.get("/checkLogin",function(req,res,next){
+router.get("/checkLogin", function (req, res, next) {
   console.log("000");
-  var param={
-    name:req.body.name,
-    pwd:req.body.pwd
+  var param = {
+    name: req.body.name,
+    pwd: req.body.pwd
   };
-  Auser.findOne(param,function(err,doc){
+  Auser.findOne(param, function (err, doc) {
     console.log("111");
-    if(err){
+    if (err) {
       res.json({
-        status:'1',
-        msg:"错啦"
+        status: '1',
+        msg: "错啦"
       })
     }
-    else{
-      if(doc){
+    else {
+      if (doc) {
         // req.session.user=doc;
         res.json({
-          status:'0',
-          msg:'',
-          result:{
-            state:'ok'
+          status: '0',
+          msg: '',
+          result: {
+            state: 'ok'
           }
         })
       }
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,50 +1,49 @@
 <template>
-  <div>
-      <div>
-          用户名：<el-input v-model="name"></el-input>
-          密码<el-input show-password v-model="pwd"></el-input>
-          <el-button @click="saveUser">登陆</el-button>
-      </div>
-      <!-- <FFF></FFF> -->
-  </div>
+    <div>
+        <div>
+            用户名：
+            <el-input v-model="name"></el-input>密码
+            <el-input show-password v-model="pwd"></el-input>
+            <el-button @click="saveUser">登陆</el-button>
+        </div>
+        <!-- <FFF></FFF> -->
+    </div>
 </template>
 
 <script>
-  import axios from 'axios'
-  import FFF from '@/components/Footer.vue'
+import axios from "axios";
+import FFF from "@/components/Footer.vue";
 
-  export default {
+export default {
     name: "gg",
-    data(){
-      return{
-        name:'',
-        pwd:''
-      }
+    data() {
+        return {
+            name: "",
+            pwd: ""
+        };
     },
-    components:{FFF},
-    methods:{
-      saveUser(){
-        let storage = window.localStorage;
-        storage.setItem('userName',this.name);
-        storage.setItem('pwd',this.pwd);
-        let params = {
-          name:this.name,
-          pwd:this.pwd
+    components: { FFF },
+    methods: {
+        saveUser() {
+            let storage = window.localStorage;
+            storage.setItem("userName", this.name);
+            storage.setItem("pwd", this.pwd);
+            let params = {
+                name: this.name,
+                pwd: this.pwd
+            };
+            axios.get("/user/checkLogin", params).then(res => {
+                console.log(res);
+                if (res.data.result.state == "ok") {
+                    this.$router.push({ name: "Home" });
+                } else {
+                    alert("用户名或密码错误");
+                }
+            });
         }
-        axios.get('/user/checkLogin',params).then(res=>{
-          console.log(res);
-          if(res.data.result.state == 'ok'){
-            this.$router.push({name:'Home'})
-          }
-          else{
-            alert('用户名或密码错误')
-          }
-        })
-      }
     }
-  }
+};
 </script>
 
 <style scoped>
-
 </style>
